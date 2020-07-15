2753a78bd5a2b0907922af2d731ba296968e1b58
vjharsh_300041725 <vijay.harsha@myntra.com>
2020-07-15 16:26:05 +0530

    first commit

---
+++ src/main/java/com/myntra/subscriptionbackend/SubscriptionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myntra.subscriptionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myntra/subscriptionbackend/SubscriptionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myntra.subscriptionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
